ea5d7327976189f4c28254e13c60de2411ddbead
Dan Allan <dallan@bnl.gov>
2020-02-05 10:38:12 -0500

    Add support for new driver discovery mechanism.

--- intake_mongo/__init__.py
+++ intake_mongo/__init__.py
@@ -3,4 +3,6 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+import intake  # Import this first to avoid circular imports during discovery.
+del intake
 from .intake_mongo import MongoDBSource
